b789038d49272b61ed81135aaa98aa8de4bb378f
viper12590 <viper1259@gmail.com>
2021-04-04 15:43:52 +0100

    no card design, postView mockup

--- frontend/src/components/post.vue
+++ frontend/src/components/post.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-if="postData.parent == null" class="card">
-    <div class="card-body">
-      <h6 v-if="postData.title" class="card-title"><a v-bind:href="'posts/' + postData.id">{{postData.title}}</a></h6>
+  <div v-if="postData.parent == null">
+    <div>
+      <h6 v-if="postData.title"><a v-bind:href="'posts/' + postData.id">{{postData.title}}</a></h6>
       <h6 v-else><a v-bind:href="'posts/' + postData.id">No title</a></h6>
       <a v-if="postData.isAnonymous">Anonymous</a>
       <a v-else>{{postData.uploader}}</a>
@@ -9,6 +9,7 @@
       <small> | No tags</small>
       <small> | 0 Comments</small>
     </div>
+    <hr>
   </div>
 </template>
 
--- frontend/src/components/postView.vue
+++ frontend/src/components/postView.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>Post view</div>
+  <div>
+    <h1>COMS10000 CS Basics Forum</h1>
+    <hr>
+    <h2>Post title</h2>
+    <div class="mb-3">
+      <a>Author</a>
+      <small class="text-muted"> | 31/12/2021 - 09:20</small>
+      <small> | No tags</small>
+      <small> | 0 Comments</small>
+    </div>
+    <div class="mb-5">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id sem sit amet tortor imperdiet gravida eget eu quam. Aliquam libero ligula, vestibulum et ultricies at, vulputate vel purus. Proin finibus neque vel blandit porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultricies accumsan justo, eu consequat risus fringilla eu. Integer id lorem pulvinar, scelerisque velit ac, hendrerit justo. Integer porta posuere odio eget congue. </p>
+    </div>
+
+    <div>
+      <p>Comments</p>
+      <hr>
+      <div>
+        <a>Author</a>
+        <small class="text-muted"> | 31/12/2021 - 09:20</small>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin id sem sit amet tortor imperdiet gravida eget eu quam. Aliquam libero ligula, vestibulum et ultricies at, vulputate vel purus. Proin finibus neque vel blandit porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultricies accumsan justo, eu consequat risus fringilla eu. Integer id lorem pulvinar, scelerisque velit ac, hendrerit justo. Integer porta posuere odio eget congue. </p>
+        <hr>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
