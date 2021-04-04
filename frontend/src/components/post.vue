6ef45a6e55813c1f664a2ed63fdb5b6bd1d3ca2b
viper12590 <viper1259@gmail.com>
2021-04-04 15:27:33 +0100

    even simpler design

--- frontend/src/components/post.vue
+++ frontend/src/components/post.vue
@@ -1,16 +1,15 @@
 <template>
-    <div v-if="postData.parent == null" class="card">
-      <div class="card-body">
-        <h6 v-if="postData.isAnonymous" class="card-title mb-0 fw-bold">Anonymous</h6>
-        <h6 v-else class="card-title mb-0 fw-bold">{{postData.uploader}}</h6>
-        <small class="text-muted">31/12/2021 - 09:20</small>
-        <p v-if="postData.title" class="card-text mb-1"><a v-bind:href="'posts/' + postData.id">{{postData.title}}</a></p>
-        <p v-else class="card-text mb-1"><a v-bind:href="'posts/' + postData.id">No title</a></p>
-        <p class="card-text">{{postData.message}}</p>
-        <p class="card-text text-end">0 Comments</p>
-        <p class="text-muted text-sm-end mb-0"><a href="#">Edit</a></p>
-      </div>
+  <div v-if="postData.parent == null" class="card">
+    <div class="card-body">
+      <h6 v-if="postData.title" class="card-title"><a v-bind:href="'posts/' + postData.id">{{postData.title}}</a></h6>
+      <h6 v-else><a v-bind:href="'posts/' + postData.id">No title</a></h6>
+      <a v-if="postData.isAnonymous">Anonymous</a>
+      <a v-else>{{postData.uploader}}</a>
+      <small class="text-muted"> | 31/12/2021 - 09:20</small>
+      <small> | No tags</small>
+      <small> | 0 Comments</small>
     </div>
+  </div>
 </template>
 
 <script>
