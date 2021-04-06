8a5d8ded31f2fc86b1767cb52a2fe4b9c5a7a338
viper12590 <viper1259@gmail.com>
2021-04-06 14:02:07 +0100

    post list show tags and date

--- frontend/src/components/post/post.vue
+++ frontend/src/components/post/post.vue
@@ -3,12 +3,16 @@
     <div>
       <h6 v-if="postData.title"><a v-bind:href="'posts/' + postData._id">{{postData.title}}</a></h6>
       <h6 v-else><a v-bind:href="'posts/' + postData._id">No title</a></h6>
-      <small>No tags</small>
+      <div v-if="postData.tags.length > 0">
+        <small v-for="tag in postData.tags" :key="tag">{{tag}} | </small>
+      </div>
+      <small v-else class="mb-0">No tags</small>
       <p>
         <a v-if="postData.anonymous">Anonymous</a>
         <a v-else>{{postData.author}}</a>
-        <small class="text-muted"> | 31/12/2021 - 09:20</small>
-        <small> | 0 Comments</small>
+        <small class="text-muted"> | {{new Intl.DateTimeFormat("en-GB",this.dateFormatOption).format(new Date(postData.createdAt))}}</small>
+        <small v-if="postData.comments.length === 1" :key="postData.comments.length">| 1 Comment</small>
+        <small v-else :key="postData.comments.length">| {{postData.comments.length}} Comments</small>
       </p>
     </div>
     <hr>
@@ -20,6 +24,14 @@ export default {
   name: "post",
   props: {
     postData: Object,
+  },
+  data() {
+    return {
+      dateFormatOption: {
+        dateStyle: "medium",
+        timeStyle: "medium"
+      }
+    }
   }
 }
 </script>
