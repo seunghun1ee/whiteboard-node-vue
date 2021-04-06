5d06d733f84c6ccb27d1c5b91771199acf0b4f77
viper12590 <viper1259@gmail.com>
2021-04-06 16:45:56 +0100

    new component recordReminder.vue

---
+++ frontend/src/components/liveSession/recordReminder.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="position-fixed bottom-0 start-0 p-3" style="z-index: 5">
+    <div class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-body">
+        This is a friendly reminder to record your lecture :)
+        <div class="mt-2 pt-2 border-top">
+          <button id="toastRecordButton" type="button" class="btn btn-danger btn-sm">Record</button>
+          <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="toast">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "recordReminder"
+}
+</script>
+
+<style scoped>
+
+</style>
